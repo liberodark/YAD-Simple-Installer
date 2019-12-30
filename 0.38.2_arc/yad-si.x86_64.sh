0c11d0c2b4f49fa527da0112cb301cd52e5d3f0a
liberodark <liberodark@gmail.com>
2019-12-30 20:55:52 +0100

    Update yad-si.x86_64.sh

--- 0.38.2_arc/yad-si.x86_64.sh
+++ 0.38.2_arc/yad-si.x86_64.sh
@@ -16,10 +16,10 @@ if ! sha1sum -c archive.fa.checksum &> /dev/null
 fi
 
 # Application name
-export appname="TinyKeep"
+export appname="Deadly Days"
 
 # Launchers icon
-export icon="tinykeep_Data/Resources/UnityPlayer.png"
+export icon="Deadly\ Days_Data/Resources/UnityPlayer.png"
 
 # Initial set
 export dir="$(dirname "$(realpath "$0")")"
@@ -44,144 +44,6 @@ mkfifo "$form_pipe"
 trap 'rm -f "$main_proc_id" "$progress_pipe" "$form_pipe"' "EXIT"
 export key="$(($RANDOM * $$))"
 export unpack='bash -c "install_app %1 %2 %3 %4"'
-
-######################################
-# Start the script in Russian locale #
-######################################
-
-if (locale | grep -e 'ru_RU'); then
-
-function install_app
-{
-
-# Form fields are read in cycles, each cycle sets new values
-# Disable all the form fields while unpacking
-echo "@disabled@" > "$form_pipe"
-echo "@disabled@" > "$form_pipe"
-echo "@disabled@" > "$form_pipe"
-echo "@disabled@" > "$form_pipe"
-
-# Unpacking from the archive
-"$fa" x "$dir"/archive.fa -o+ -dp"$1" >> "$progress_pipe" \
-2>&1 >> "$progress_pipe" & echo "$!" >> "$main_proc_id"
-
-# Wait here and it returns exit status "${?}"
-# It's stderr redirected to /dev/null for prevent messages from the kill
-wait "$(<$main_proc_id)" 2>/dev/null
-
-if [ "$?" = "0" ]; then
-	echo "#Установка завершена" >> "$progress_pipe"
-	kill "$main_pid" 2>/dev/null
-	>"$main_proc_id"
-else
-	echo "#Ошибка установки" >> "$progress_pipe"
-	kill "$main_pid" 2>/dev/null
-	>"$main_proc_id"
-fi
-
-# Create launchers in the applications menu and/or on the desktop
-if [ "$2" = "TRUE" ]; then
-	[ ! -d "$menu_dir" ] && mkdir -p "$menu_dir"
-	cat << EOF > "$menu_launcher"
-[Desktop Entry]
-Name=$appname
-Exec="$1/$appname/run.sh"
-Icon=$1/$appname/$icon
-Type=Application
-Categories=Game;
-StartupNotify=true
-Comment=Start $appname
-Comment[ru_RU]=Запустить $appname
-EOF
-	chmod +x "$menu_launcher"
-fi
-
-if [ "$3" = "TRUE" ]; then
-	cat << EOF > "$desktop_launcher"
-[Desktop Entry]
-Name=$appname
-Exec="$1/$appname/run.sh"
-Icon=$1/$appname/$icon
-Type=Application
-Categories=Game;
-StartupNotify=true
-Comment=Start $appname
-Comment[ru_RU]=Запустить $appname
-EOF
-	chmod +x "$desktop_launcher"
-fi
-}
-
-export -f install_app
-
-function get_pid_and_kill
-{
-sure_command='"$yad" --title=" " --width=1 \
-	--text="Отменить установку?" \
-	--text-align=center --on-top --center \
-	--window-icon="system-software-install" \
-	--button="Да!gtk-yes:0" --button="Нет!gtk-no:1"'
-sure_command_pid="$(ps -eo pid,cmd | grep -F "$sure_command" | grep -v \
-"grep" | awk '{ print $1 }')"
-
-if [ -s "$main_proc_id" ] && [ "$sure_command_pid" = "" ] && \
-	"$yad" --title=" " --width=1 \
-	--text="Отменить установку?" \
-	--text-align=center	--on-top --center \
-	--window-icon="system-software-install" \
-	--button="Да!gtk-yes:0" --button="Нет!gtk-no:1"; then
-	if [ -s "$main_proc_id" ]; then
-		bckupid="$(<$main_proc_id)"
-		>"$main_proc_id"
-		kill "$bckupid" 2>/dev/null
-	fi
-
-# Special YAD variable "$YAD_PID" stores the main window PID
-# Killing that PID closes the window
-	[ "$1" = "CLOSE" ] && kill -s SIGUSR2 "$YAD_PID"
-elif [ ! -s "$main_proc_id" ]; then
-	[ "$1" = "CLOSE" ] && kill -s SIGUSR2 "$YAD_PID"
-fi
-}
-
-export -f get_pid_and_kill
-
-exec 3<> "$progress_pipe"
-exec 4<> "$form_pipe"
-
-"$yad" --plug="$key" --tabnum=1 --form \
---field=" Выберите путь для установки::DIR" \
---image="$dir"/header.jpg --image-on-top \
---field=" Создать значок запуска в меню приложений:CHK" \
---field=" Создать значок запуска на рабочем столе:CHK" \
---field=" Начать установку!system-software-install:fbtn" \
---cycle-read <&4 &
-
-# Initial cycle that sets the form field values
-echo "$HOME" > "$form_pipe"		# default directory
-echo "TRUE" > "$form_pipe"		# default first checkbox value
-echo "FALSE" > "$form_pipe"		# default second checkbox value
-echo "$unpack &" > "$form_pipe"	# progress bar value
-
-"$yad" --plug="$key" --tabnum=2 --progress <&3 &
-
-"$yad" --paned --key="$key" --buttons-layout=center \
---button=" Выйти!gtk-quit":'bash -c "get_pid_and_kill CLOSE" 2>/dev/null' \
---title="$appname: YAD Simple Installer" \
---window-icon="system-software-install" --fixed \
---center --no-escape --undecorated & main_pid="$!"
-
-# Redirecting kill output from get_pid_and_kill function to /dev/null
-wait "$main_pid" 2>/dev/null
-
-exec 3>&-
-exec 4>&-
-exit 0
-fi
-
-#######################################################
-# Start the script in English for non-Russian locales #
-#######################################################
 
 function install_app
 {
@@ -223,7 +85,6 @@ Type=Application
 Categories=Game;
 StartupNotify=true
 Comment=Start $appname
-Comment[ru_RU]=Запустить $appname
 EOF
 	chmod +x "$menu_launcher"
 fi
@@ -238,7 +99,6 @@ Type=Application
 Categories=Game;
 StartupNotify=true
 Comment=Start $appname
-Comment[ru_RU]=Запустить $appname
 EOF
 	chmod +x "$desktop_launcher"
 fi
